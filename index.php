56f6f5ca6ab0bb21c686f5af2d4d975cc1400d0f
renansanfelice <36107272+renansanfelice@users.noreply.github.com>
2018-02-20 22:14:59 -0300

    alterei o index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Teste PHP</title>
  </head>
  <body>
- <?php echo "<p>Olá <b>Renan C Sanfelice!</b></n></p>"; ?>
+ <?php echo "<p>Olá <b>Renan Cesar Sanfelice!</b></n></p>"; ?>
  </body>
 </html>
