57e78e8a6eca7594705720fe0a2274a5bd25a351
Renan C Sanfelice <renan@emporiumdigital.net>
2018-03-08 23:25:57 -0300

    Aulas 37 a 39 - usando Data e Hora ho PHP

---
+++ date/exemplo-02.php
@@ -0,0 +1,7 @@
+<?php
+
+$ts = strtotime('2001-09-11');
+
+echo "<br>";
+
+echo date("l, d/m/Y H:i:s", $ts);
---
+++ date/exemplo-03.php
@@ -0,0 +1,5 @@
+<?php
+
+setlocale(LC_ALL,"pt-BR","pt-BR.utf-8", "portuguese");
+
+echo ucwords(strftime("%A %B"));
