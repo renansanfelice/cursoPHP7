f0896a8223367723af5b232d6810b0e8909d0836
Renan C Sanfelice <renan@emporiumdigital.net>
2018-07-18 23:11:55 -0300

    ate aula 71 - manupulacao de arquivos

---
+++ fgets/exemplo-02.php
@@ -0,0 +1,17 @@
+<?php
+
+$filename = "logo.png";
+
+$base64 = base64_encode(file_get_contents($filename));
+
+$fileinfo = new finfo(FILEINFO_MIME_TYPE);
+
+$mimetype= $fileinfo->file($filename);
+
+$base64encode = "data=:" . $mimetype . ";base64," . $base64;
+
+?>
+
+<a ref="<?=$base64encode?>" target="blank">Link para imagem</a>
+<img src="<?=$base64encode?>">
+
